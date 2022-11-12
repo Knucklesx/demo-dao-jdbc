d5e4e0fd70bf19db8f55f24b7c89f3b771e4de3c
Knucklesx <gcfacchi@gmail.com>
2022-11-12 09:25:03 -0300

    FEAT - implemented delete logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 	public static void main(String[] args) {
 
+		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== TEST 1: seller findById ===");
@@ -39,7 +42,7 @@ public class Program {
 		
 		System.out.println("=== TEST 4: seller sellerInsert ===");
 		
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller(null, "Greg2", "greg@gmail.com", new Date(), 4000.0, department);
 		
 		sellerDao.insert(newSeller);
 		
@@ -55,6 +58,16 @@ public class Program {
 		
 		System.out.println("Update Completed!");
 		
+		
+		System.out.println("=== TEST 5: seller sellerDelete ===");
+		
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		
+		System.out.println("Delete completed!");
+		
+		sc.close();
 		
 	}
 }
